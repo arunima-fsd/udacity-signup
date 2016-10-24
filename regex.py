70a41ce36b165c59f30af288f1e863cd1a2dd37a
Arunima Srivastava <arunimaasrivastava@gmail.com>
2016-10-24 20:12:45 +0530

    Add the python file for input validation

---
+++ regex.py
@@ -0,0 +1,27 @@
+
+class Validate:
+    USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+    PASS_RE = re.compile(r"^.{3,20}$")
+    EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+    def __init__(self, username, password, verpassword, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        slef.verpassword = verpassword
+
+    def valid_username(self, username):
+        if(USER_RE.match(self.username)):
+            return True
+
+    def valid_password(self, password, verpassword):
+        if(PASS_RE.match(self.password)):
+            return True
+
+    def valid_email(self, email):
+        if(EMAIL_RE.match(self.email)):
+            return True
+
+    def same_password(self, password, verpassword):
+        if(self.password == self.verpassword):
+            return True
